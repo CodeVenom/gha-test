01124a00377b95baa9d4110e7798be4636bdc86c
CodeVenom <juri.jaeger@gmail.com>
2022-04-28 20:15:53 +0200

    test

--- .github/workflows/jj.sh
+++ .github/workflows/jj.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-# TODO: introduce functions and call em with "$@"
-# TODO: use arguments instead of GITHUB_SHA and GITHUB_ENV
-# TODO: do not check for tf files if workflow files have been changed, set [] instead
-
-# find workflow and tf file changes
-#JJ_A=$(git diff "${GITHUB_SHA}^" --name-only)
-#JJ_B=$(echo "$JJ_A" | grep '.github/workflows' | head -1)
-#JJ_C=$(echo "$JJ_A" | grep -E '[-a-zA-Z0-9]+\.tf' | sed -E 's%/[-a-zA-Z0-9\.]+%%g' | sort -u | jq -R -s -c 'split("\n")[:-1]')
-#
-#echo "::set-output name=workflows::$JJ_B"
-#echo "::set-output name=prefix-list::$JJ_C"
-
 prepare() {
   # find workflow and tf file changes
   JJ_A=$(git diff "${GITHUB_SHA}^" --name-only)
-  JJ_B=$(echo "$JJ_A" | grep '.github/workflows' | head -1)
-  JJ_C=$(echo "$JJ_A" | grep -E '[-a-zA-Z0-9]+\.tf' | sed -E 's%/[-a-zA-Z0-9\.]+%%g' | sort -u | jq -R -s -c 'split("\n")[:-1]')
+  JJ_B=$(f1 "$JJ_A")
+  # TODO: do not check for tf files if workflow files have been changed, set [] instead
+  JJ_C=$(f2 "$JJ_A")
 
   echo "::set-output name=workflows::$JJ_B"
   echo "::set-output name=prefix-list::$JJ_C"
+}
+
+f1() {
+  # TODO: add guard
+  echo "$1" | grep '.github/workflows' | head -1
+}
+
+f2() {
+  # TODO: add guard
+  echo "$1" | grep -E '[-a-zA-Z0-9]+\.tf' | sed -E 's%/[-a-zA-Z0-9\.]+%%g' | sort -u | jq -R -s -c 'split("\n")[:-1]'
 }
 
 if declare -f "$1" > /dev/null

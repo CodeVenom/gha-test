4c54e991c7960705458306a7f92b1cc42feb381e
CodeVenom <juri.jaeger@gmail.com>
2022-04-28 18:13:53 +0200

    test

--- .github/workflows/jj.sh
+++ .github/workflows/jj.sh
@@ -2,7 +2,7 @@
 
 # find workflow and tf file changes
 JJ_A=$(git diff "${GITHUB_SHA}^" --name-only)
-JJ_B=$(echo "$JJ_A" | grep '.github/workflows')
+JJ_B=$(echo "$JJ_A" | grep '.github/workflows' | head -1)
 JJ_C=$(echo "$JJ_A" | grep -E '[-a-zA-Z0-9]+\.tf' | sed -E 's%/[-a-zA-Z0-9\.]+%%g' | sort -u | jq -R -s -c 'split("\n")[:-1]')
 
 echo "JJ_WORKFLOW=$JJ_B" >> $GITHUB_ENV

443974b0153737f61dcb4b737134e9cde40918cc
CodeVenom <juri.jaeger@gmail.com>
2022-04-28 18:05:11 +0200

    test

--- .github/workflows/jj.sh
+++ .github/workflows/jj.sh
@@ -4,5 +4,5 @@ JJ_A=$(git diff "${GITHUB_SHA}^" --name-only)
 JJ_B=$(echo "$JJ_A" | grep '.github/workflows')
 JJ_C=$(echo "$JJ_A" | grep -E '[-a-zA-Z0-9]+\.tf' | sed -E 's%/[-a-zA-Z0-9\.]+%%g' | sort -u | jq -R -s -c 'split("\n")[:-1]')
 
-echo "$JJ_B"
-echo "$JJ_C"
+echo "JJ_WORKFLOW=$JJ_B" >> $GITHUB_ENV
+echo "JJ_TF=$JJ_C" >> $GITHUB_ENV

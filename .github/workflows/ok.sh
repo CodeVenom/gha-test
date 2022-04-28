2d4dc8361f68194286c62ba001d6ba7d88b9d035
CodeVenom <juri.jaeger@gmail.com>
2022-04-28 18:58:35 +0200

    test

--- .github/workflows/ok.sh
+++ .github/workflows/ok.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo "::set-output name=blub::$(echo "$1" | jq '. | length')"
+echo "::set-output name=bla::$(echo "$1" | jq '. | length')"
+echo "::set-output name=blub::$(echo "$2" | jq '. | length')"

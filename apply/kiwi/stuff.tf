1cce7bd6c4a1c8e62b6b7e0f0e485524ed91a368
CodeVenom <juri.jaeger@gmail.com>
2022-05-02 20:30:20 +0200

    test

--- apply/kiwi/stuff.tf
+++ apply/kiwi/stuff.tf
@@ -4,6 +4,6 @@ resource "aws_instance" "web" {
 
   tags = {
     project = "kiwi"
-    foo = "barbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbar"
+    foo = "barbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbarbar"
   }
 }
